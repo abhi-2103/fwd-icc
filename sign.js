c95608e3c67d9b6601e86d496c85d4e165d48d61
abhi-2103 <kulkaraniabhilash@gmail.com>
2026-03-12 21:04:36 +0530

    Update sign.js

--- sign.js
+++ sign.js
@@ -1,18 +1,18 @@
-function login()
-{
+function login() {
 
-let user = document.getElementById("username").value;
-let pass = document.getElementById("password").value;
+    let user = document.getElementById("username").value;
+    let pass = document.getElementById("password").value;
 
-if(user === "icc" && pass === "cricket123")
-{
-document.getElementById("message").innerHTML="Login Successful";
-document.getElementById("message").style.color="green";
-}
-else
-{
-document.getElementById("message").innerHTML="Invalid Username or Password";
-document.getElementById("message").style.color="red";
-}
+    if (user === "icc" && pass === "cricket123") {
+        document.getElementById("message").innerHTML = "Login Successful";
+        document.getElementById("message").style.color = "green";
 
+        // redirect to home after 1 second
+        setTimeout(function () {
+            window.location.href = "index.html";   // or "./index.html"
+        }, 1000);
+    } else {
+        document.getElementById("message").innerHTML = "Invalid Username or Password";
+        document.getElementById("message").style.color = "red";
+    }
 }
